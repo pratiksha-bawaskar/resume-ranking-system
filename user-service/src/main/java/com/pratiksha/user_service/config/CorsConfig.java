82755f665b40b5668418c0276ba1b904f7ef8fc7
Pratiksha Bawaskar <bawaskarpratiksha8@gmail.com>
2026-03-21 15:57:06 +0530

    fixed cors issue

--- user-service/src/main/java/com/pratiksha/user_service/config/CorsConfig.java
+++ user-service/src/main/java/com/pratiksha/user_service/config/CorsConfig.java
@@ -14,8 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3001")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
                         .allowedHeaders("*");
             }
         };
