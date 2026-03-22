19825480abe6396c6fcc458ad79a13b539c6c64e
Pratiksha Bawaskar <bawaskarpratiksha8@gmail.com>
2026-03-22 17:37:19 +0530

    FInal Fix User Service CORS

--- user-service/src/main/java/com/pratiksha/user_service/controller/UserController.java
+++ user-service/src/main/java/com/pratiksha/user_service/controller/UserController.java
@@ -11,16 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.pratiksha.user_service.entity.User;
 
-@CrossOrigin(origins = "https://resume-ranking-app.netlify.app")
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @CrossOrigin(origins = "https://resume-ranking-app.netlify.app")
     @PostMapping
     public User createUser(@RequestBody User user) {
         return user;
     }
 
+    @CrossOrigin(origins = "https://resume-ranking-app.netlify.app")
     @GetMapping
     public List<User> getUsers() {
         return List.of();
