e7cd65b966d802023a48e4021d2cee8ec548f6b0
Pratiksha Bawaskar <bawaskarpratiksha8@gmail.com>
2026-03-22 17:47:30 +0530

    Fix OPTIONSpreflight CORS issue

--- user-service/src/main/java/com/pratiksha/user_service/controller/UserController.java
+++ user-service/src/main/java/com/pratiksha/user_service/controller/UserController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pratiksha.user_service.entity.User;
-
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -25,5 +25,11 @@ public class UserController {
     @GetMapping
     public List<User> getUsers() {
         return List.of();
+    }
+
+    // 🔥 IMPORTANT: handle preflight request
+    @RequestMapping(method = RequestMethod.OPTIONS)
+    public void handleOptions() {
+        // empty but required
     }
 }
