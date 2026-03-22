10d04f0f68f7c8e347534fece88503605cb3c318
Pratiksha Bawaskar <bawaskarpratiksha8@gmail.com>
2026-03-22 18:09:29 +0530

    Fixed duplicated CORS config

--- user-service/src/main/java/com/pratiksha/user_service/config/CorsConfig.java
+++ user-service/src/main/java/com/pratiksha/user_service/config/CorsConfig.java
@@ -1,22 +1,20 @@
 package com.pratiksha.user_service.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-
                 registry.addMapping("/**")
                         .allowedOrigins("https://resume-ranking-app.netlify.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("*")
                         .allowedHeaders("*");
             }
         };
