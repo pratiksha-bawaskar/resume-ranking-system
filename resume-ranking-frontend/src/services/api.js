00f12fed8dfb987242bb6dbce838704193972048
Pratiksha Bawaskar <bawaskarpratiksha8@gmail.com>
2026-03-23 18:39:12 +0530

    fixed api to gateway

--- resume-ranking-frontend/src/services/api.js
+++ resume-ranking-frontend/src/services/api.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-const USER_URL = "https://user-service-5k6b.onrender.com";
+const USER_URL = "https://api-gateway-357r.onrender.com/api/users";
 const RANKING_URL = "https://resume-ranking-system-ux0j.onrender.com";
 const RESUME_URL = "https://resume-service-y9za.onrender.com";
 
-// ✅ CREATE USER
+// CREATE USER
 export const createUser = async (user) => {
-  const res = await axios.post(`${USER_URL}/users`, user);
+  const res = await axios.post(USER_URL, user);
   return res.data;
 };
 
-// ✅ UPLOAD RESUME
+// UPLOAD RESUME
 export const uploadResume = async (formData) => {
   const res = await axios.post(
     `${RESUME_URL}/resume/upload`,
@@ -24,13 +24,13 @@ export const uploadResume = async (formData) => {
   return res.data;
 };
 
-// ✅ GET RANKING
+// GET RANKING
 export const getRanking = async () => {
   const res = await axios.get(`${RANKING_URL}/ranking`);
   return res.data;
 };
 
-// ✅ DOWNLOAD REPORT
+// DOWNLOAD REPORT
 export const downloadReportAPI = async () => {
   const response = await fetch(`${RANKING_URL}/ranking/download-report`);
   return await response.blob();
