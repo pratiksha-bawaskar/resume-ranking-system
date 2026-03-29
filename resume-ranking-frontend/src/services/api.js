db6ebdc5761a14c7843ddc456e9eda7818686975
Pratiksha Bawaskar <bawaskarpratiksha8@gmail.com>
2026-03-29 12:19:05 +0530

    Integrated API Gateway properly

--- resume-ranking-frontend/src/services/api.js
+++ resume-ranking-frontend/src/services/api.js
@@ -1,25 +1,24 @@
 import axios from "axios";
 
-// 🔥 DIRECT SERVICES (no gateway for now)
-const USER_URL = "https://user-service-5k5b.onrender.com/users";
-const RANKING_URL = "https://resume-ranking-system-ux0j.onrender.com";
-const RESUME_URL = "https://resume-service-y9za.onrender.com";
+// ✅ API GATEWAY URL (replace with your real one)
+const BASE_URL = "https://api-gateway-357r.onrender.com";
+
+// ✅ ALL CALLS THROUGH GATEWAY
+const USER_URL = `${BASE_URL}/api/users`;
+const RESUME_URL = `${BASE_URL}/resume`;
+const RANKING_URL = `${BASE_URL}/ranking`;
+
 
 // CREATE USER
 export const createUser = async (user) => {
-  try {
-    const res = await axios.post(USER_URL, user);
-    return res.data;
-  } catch (error) {
-    console.error("CREATE USER ERROR:", error);
-    throw new Error("Error creating user");
-  }
+  const res = await axios.post(USER_URL, user);
+  return res.data;
 };
 
 // UPLOAD RESUME
 export const uploadResume = async (formData) => {
   const res = await axios.post(
-    `${RESUME_URL}/resume/upload`,
+    `${RESUME_URL}/upload`,
     formData,
     {
       headers: {
@@ -32,12 +31,12 @@ export const uploadResume = async (formData) => {
 
 // GET RANKING
 export const getRanking = async () => {
-  const res = await axios.get(`${RANKING_URL}/ranking`);
+  const res = await axios.get(RANKING_URL);
   return res.data;
 };
 
 // DOWNLOAD REPORT
 export const downloadReportAPI = async () => {
-  const response = await fetch(`${RANKING_URL}/ranking/download-report`);
+  const response = await fetch(`${RANKING_URL}/download-report`);
   return await response.blob();
 };
